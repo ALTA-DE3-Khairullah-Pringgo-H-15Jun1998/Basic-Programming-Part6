8a5d593bb51aac1fa0e47649e935bd8c4fc074e3
IrulJJ2 <vicbuild14@gmail.com>
2024-05-23 22:16:00 +0700

    Problem 1 - 5

--- problem3/main.py
+++ problem3/main.py
@@ -1,10 +1,14 @@
 def array_unique(arrayA, arrayB):
-    # your code here
-    return ''
+    unique_elements = []
+    setB = set(arrayB)
+    for num in arrayA:
+        if num not in setB:
+            unique_elements.append(num)
+    return unique_elements
 
 if __name__ == '__main__':
-    print(array_unique([1, 2, 3, 4], [1, 3, 5, 10, 16])) # [2, 4]
-    print(array_unique([10, 20, 30, 40], [5, 10, 15, 59])) # [20, 30, 40]
-    print(array_unique([1, 3, 7], [1, 3, 5])) # [7]
-    print(array_unique([3, 8], [2, 8])) # [3]
-    print(array_unique([1, 2, 3], [3, 2, 1])) # []
+    print(array_unique([1, 2, 3, 4], [1, 3, 5, 10, 16]))  # Output: [2, 4]
+    print(array_unique([10, 20, 30, 40], [5, 10, 15, 59]))  # Output: [20, 30, 40]
+    print(array_unique([1, 3, 7], [1, 3, 5]))  # Output: [7]
+    print(array_unique([3, 8], [2, 8]))  # Output: [3]
+    print(array_unique([1, 2, 3], [3, 2, 1]))  # Output: []
